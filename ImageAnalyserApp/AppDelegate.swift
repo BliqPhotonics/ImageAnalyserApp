31de7862b777671001d804a5a556ce3244abf8ee
stevebegin <stevebegin@gmail.com>
2018-03-22 09:26:38 -0400

    Initial Commit

---
+++ ImageAnalyserApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ImageAnalyserApp
+//
+//  Created by Steve Begin on 2018-03-22.
+//  Copyright © 2018 BLIQc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ImageAnalyserApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageAnalyserApp
+//
+//  Created by Steve Begin on 2018-03-22.
+//  Copyright © 2018 BLIQc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
